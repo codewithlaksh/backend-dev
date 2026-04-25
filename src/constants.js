6313e177a44e2c602b62f920d4453071134aadfb
Lakshyaraj Dash <195118589+codewithlaksh@users.noreply.github.com>
2026-04-25 19:50:57 +0530

    Module 4 - Connect to MongoDB

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const MONGODB_URI = process.env.MONGODB_URI;
+export const NODE_ENV = process.env.NODE_ENV;
---
+++ src/lib/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { MONGODB_URI, NODE_ENV } from "../constants.js";
+
+export const connectDB = async () => {
+  // if (!MONGODB_URI) throw new Error("Error: Please provide MongoDB uri!");
+  try {
+    const connection = await mongoose.connect(MONGODB_URI);
+    if (NODE_ENV !== "production")
+      console.log(
+        `Connected to MongoDB !! Host: ${connection.connections[0].host}`,
+      );
+  } catch (error) {
+    throw error;
+  }
+};
