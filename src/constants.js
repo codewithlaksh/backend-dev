8909fafa59ff9d9ba483f6d5d281258d5c2d6ad1
Lakshyaraj Dash <195118589+codewithlaksh@users.noreply.github.com>
2026-04-26 06:35:10 +0530

    Module 5 - User & Profile Models

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const NODE_ENV = process.env.NODE_ENV;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
